d1e41ae2e1ca81e54bd4485ec4df8857c9663374
Apurva <32777604+apurva-s@users.noreply.github.com>
2021-12-04 18:27:23 -0500

    Update enterRoom.test.ts

--- backend/__tests__/enterRoom.test.ts
+++ backend/__tests__/enterRoom.test.ts
@@ -1,89 +1,89 @@
-// import { Prisma, PrismaClient } from '@prisma/client'
-// import { createMcqQuestionCore } from '../src/utils/createMcqQuestionCore'
-// import { createRoomCore } from '../src/utils/createRoomCore'
-// import { enterRoomCore } from '../src/utils/enterRoomCore';
+import { Prisma, PrismaClient } from '@prisma/client'
+import { createMcqQuestionCore } from '../src/utils/createMcqQuestionCore'
+import { createRoomCore } from '../src/utils/createRoomCore'
+import { enterRoomCore } from '../src/utils/enterRoomCore';
 
-// // Add custom matcher to namespace to avoid TS errors
-// declare global {
-//   namespace jest {
-//     interface Matchers<R> {
-//       toBeValidEnterRoomResponse(): CustomMatcherResult
-//     }
-//   }
-// }
+// Add custom matcher to namespace to avoid TS errors
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toBeValidEnterRoomResponse(): CustomMatcherResult
+    }
+  }
+}
 
-// let prisma: PrismaClient<
-//   Prisma.PrismaClientOptions,
-//   never,
-//   Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
-// >
+let prisma: PrismaClient<
+  Prisma.PrismaClientOptions,
+  never,
+  Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
+>
 
-// // Player ID of the host user
-// let playerId: string;
-// // Key of room that user will join
-// let roomKey: string;
+// Player ID of the host user
+let playerId: string;
+// Key of room that user will join
+let roomKey: string;
 
-// beforeAll(async () => {
-//   // Initialize db connection
-//   prisma = new PrismaClient()
+beforeAll(async () => {
+  // Initialize db connection
+  prisma = new PrismaClient()
 
-//   const { id:hostPlayerId } = await prisma.player.create({
-//     data: {
-//       name: 'foo'
-//     },
-//     select: {
-//       id: true,
-//     }
-//   })
+  const { id:hostPlayerId } = await prisma.player.create({
+    data: {
+      name: 'foo'
+    },
+    select: {
+      id: true,
+    }
+  })
 
-//   const createRoomResponse = await createRoomCore(hostPlayerId, 'foo', 'bar', prisma);
+  const createRoomResponse = await createRoomCore(hostPlayerId, 'foo', 'bar', prisma);
 
-//   const {id} = await prisma.player.create({
-//     data: {
-//       name: 'non host player',
-//     },
-//     select: {
-//       id: true,
-//     }
-//   })
+  const {id} = await prisma.player.create({
+    data: {
+      name: 'non host player',
+    },
+    select: {
+      id: true,
+    }
+  })
 
-//   let roomId = createRoomResponse.room.id;
-//   let roomKey = createRoomResponse.room.key;
-//   playerId = id;
+  let roomId = createRoomResponse.room.id;
+  let roomKey = createRoomResponse.room.key;
+  playerId = id;
 
-// })
+})
 
-// afterAll(() => {
-//   // Cleanup db connection
-//   prisma.$disconnect()
-// })
+afterAll(() => {
+  // Cleanup db connection
+  prisma.$disconnect()
+})
 
-// // Create custom matcher to test a valid function reponse
-// expect.extend({
-//   toBeValidEnterRoomResponse(response) {
-//     if (
-//       response &&
-//       response.token &&
-//       response.questionData &&
-//       Array.isArray(response.questionData.mcqQuestions)
-//     ) {
-//       return {
-//         pass: true,
-//         message: () => 'Valid response',
-//       }
-//     }
+// Create custom matcher to test a valid function reponse
+expect.extend({
+  toBeValidEnterRoomResponse(response) {
+    if (
+      response &&
+      response.token &&
+      response.questionData &&
+      Array.isArray(response.questionData.mcqQuestions)
+    ) {
+      return {
+        pass: true,
+        message: () => 'Valid response',
+      }
+    }
 
-//     return {
-//       pass: false,
-//       message: () => 'Invalid response',
-//     }
-//   },
-// })
+    return {
+      pass: false,
+      message: () => 'Invalid response',
+    }
+  },
+})
 
-// test('enter room with proper parameters', async () => {
-//   const res = await enterRoomCore(playerId, 'foo', roomKey, prisma);
-//   expect(res).toBeValidEnterRoomResponse();
-// })
+test('enter room with proper parameters', async () => {
+  const res = await enterRoomCore(playerId, 'foo', roomKey, prisma);
+  expect(res).toBeValidEnterRoomResponse();
+})
 
 // test('enter room with invalid playerId', async () => {
 //   await expect(enterRoomCore('----', 'foo', roomKey, prisma)).rejects.toThrowError();
