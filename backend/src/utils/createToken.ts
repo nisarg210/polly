7ecc8de5211169d9626c353ea5f0b2c321e65dc6
Apurva <32777604+apurva-s@users.noreply.github.com>
2021-12-04 19:04:56 -0500

    Update createToken.ts

--- backend/src/utils/createToken.ts
+++ backend/src/utils/createToken.ts
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
 export function createToken(playerId: string): string {
-  return jwt.sign({playerId}, process.env.JWT_SECRET!);
+  //return jwt.sign({playerId}, process.env.JWT_SECRET!);
+  return jwt.sign({playerId}, 'secret');
 }
